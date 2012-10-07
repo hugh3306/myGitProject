41c482515e26745c2b6e27409a8833b32e52c692
Li MinWei <hugh3306@li-minweis-computer.local>
2012-10-07 19:31:07 +0800

    [Note] add function insert_in_front

--- linklist/linklist.c
+++ linklist/linklist.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "linklist.h"
+
+int insert_in_front(ListElement **head, int data) {
+	ListElement *newElement = (ListElement*)malloc(sizeof(ListElement));
+	if(NULL == newElement) {
+		printf("%s : malloc fail", __func__);
+		return -1;
+	}
+
+	newElement->data = data;
+	newElement->next = *head;
+	*head = newElement;
+
+	return 0;
+}
 
 int main() {
 	printf("Hello World!!\n");
